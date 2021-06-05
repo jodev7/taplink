c89b66854d46cd496006cebedddf3cdfb19fe6cc
YusufDEV77 <gtarprp002@gmail.com>
2021-06-05 16:03:16 +0500

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Switch } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import './App.css'
 
 
-ReactDOM.render(
-
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+    <HashRouter>
+      <Switch>
+        <Route path='/' component={App}/>
+      </Switch>
+    </HashRouter>
+), document.getElementById('root'));
