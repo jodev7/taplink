917d0dc7b25d09bd5c4ca55ac093d1c2b85a0351
YusufDEV77 <gtarprp002@gmail.com>
2021-06-05 16:54:34 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
 function App() {
-  return (
+  return (  //https://www.youtube.com/channel/UC5r7DW3zTlzU9mfqhZVKSKw
     <div className="App">
-      hello
+      <div className='header'>
+            <h1 className='logo'>DavronNo1</h1>
+        </div>
+        <div className='media'>
+        <a className='aaa' href="https://www.donationalerts.com/c/davronno1" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/cotton/100/000000/donate--v1.png"/>
+        <h3>ДОНАТ</h3>
+        </a>
+        </div>
+        <div className='media'>
+        <a className='aaa' href="https://www.youtube.com/channel/UC5r7DW3zTlzU9mfqhZVKSKw" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/color/100/000000/youtube--v3.png"/>
+        <h3>Youtube</h3>
+        </a>
+        </div>
+        <div className='media'>
+        <a className='aaa' href="https://www.tiktok.com/@davronno1?lang=ru-RU" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/doodle/100/000000/tiktok--v2.png"/>
+        <h3>Tik Tok</h3>
+        </a>
+        </div>
+        <div className='media'>
+        <a className='aaa' href="https://www.instagram.com/davron.no1/" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/cute-clipart/100/000000/instagram-new.png"/>
+        <h3>Instagram</h3>
+        </a>
+        </div>
+        <div className='media'>
+        <a className='aaa' href="https://discord.gg/YPjw3RZjQd" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/cute-clipart/100/000000/discord-logo.png"/>
+        <h3>Discord</h3>
+        </a>
+        </div>
+        
     </div>
   );
 }
