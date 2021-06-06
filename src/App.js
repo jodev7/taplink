c2f1ce5562e0ffab425e966012d88d77926592ff
YusufDEV77 <gtarprp002@gmail.com>
2021-06-06 05:24:08 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 import React from 'react'
 
 function App() {
-  return (  //https://www.youtube.com/channel/UC5r7DW3zTlzU9mfqhZVKSKw
-    <div className="App">
-      <div className='header'>
-            <h1 className='logo'>DavronNo1</h1>
-        </div>
-        <div className='media'>
+  return (  //
+    <div className="container">
+        <div className='row'>
+        <div className='col'>
         <a className='aaa' href="https://www.donationalerts.com/c/davronno1" style={{ textDecoration: 'none' }}>
-        <img src="https://img.icons8.com/cotton/100/000000/donate--v1.png"/>
+        <img src="https://img.icons8.com/cotton/100/000000/donate--v1.png" alt=""/>
         <h3>ДОНАТ</h3>
         </a>
         </div>
-        <div className='media'>
+        <div className='col'>
         <a className='aaa' href="https://www.youtube.com/channel/UC5r7DW3zTlzU9mfqhZVKSKw" style={{ textDecoration: 'none' }}>
-        <img src="https://img.icons8.com/color/100/000000/youtube--v3.png"/>
+        <img src="https://img.icons8.com/color/100/000000/youtube--v3.png" alt=""/>
         <h3>Youtube</h3>
         </a>
         </div>
-        <div className='media'>
+        <div className='col'>
         <a className='aaa' href="https://www.tiktok.com/@davronno1?lang=ru-RU" style={{ textDecoration: 'none' }}>
-        <img src="https://img.icons8.com/doodle/100/000000/tiktok--v2.png"/>
+        <img src="https://img.icons8.com/doodle/100/000000/tiktok--v2.png" alt=""/>
         <h3>Tik Tok</h3>
         </a>
         </div>
-        <div className='media'>
+        <div className='col'>
         <a className='aaa' href="https://www.instagram.com/davron.no1/" style={{ textDecoration: 'none' }}>
-        <img src="https://img.icons8.com/cute-clipart/100/000000/instagram-new.png"/>
+        <img src="https://img.icons8.com/cute-clipart/100/000000/instagram-new.png" alt=""/>
         <h3>Instagram</h3>
         </a>
         </div>
-        <div className='media'>
+        <div className='col'>
         <a className='aaa' href="https://discord.gg/YPjw3RZjQd" style={{ textDecoration: 'none' }}>
-        <img src="https://img.icons8.com/cute-clipart/100/000000/discord-logo.png"/>
+        <img src="https://img.icons8.com/cute-clipart/100/000000/discord-logo.png" alt=""/>
         <h3>Discord</h3>
         </a>
         </div>
-        
+        <div className='col'>
+        <a className='aaa' href="https://t.me/DavronNo1" style={{ textDecoration: 'none' }}>
+        <img src="https://img.icons8.com/cute-clipart/100/000000/telegram-app.png" alt=""/>
+        <h3>Telegram</h3>
+        </a>
+        </div>
+            <h1 className='text-center'>DavronNo1</h1>
+        </div>
     </div>
   );
 }
 
 export default App;
+// //<div className='header'>
+// <h1 className='logo'>DavronNo1</h1>
+// </div>
